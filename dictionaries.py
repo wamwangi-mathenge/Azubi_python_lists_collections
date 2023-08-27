5dbfa3b2e3cd7b9723d1ebe052cd2908b9d2275f
Brian <wamwangimathenge@gmail.com>
2023-08-27 10:48:56 +0300

    Append dictionaries onto a list

--- dictionaries.py
+++ dictionaries.py
@@ -1,7 +1,19 @@
 # Dictionaries => key-value pairs
 # Dictionaries does not guarantee order
 
-person = {"first": "Christopher"}
-person["last"] = "Harrison"
-print(person)
-print(person["first"])
+person1 = {"first": "Christopher"}
+person1["last"] = "Harrison"
+print(person1)
+print(person1["first"])
+print()
+
+person2 = {"first": "Brian"}
+person2["last"] = "Mathenge"
+print(person2)
+
+print()
+
+people = []
+people.append(person1)
+people.append(person2)
+print(people)
